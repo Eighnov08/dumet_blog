7f24ce49d6956917c7ae86e43e39006a00af942a
unknown <fajarseptiadi>
2019-06-24 11:00:00 +0700

    menambahkan folder dumet_blog ke git hub

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "dumet_blog";
+
+    $connection = mysqli_connect($servername,$username,$password,$dbname);
+
+    if(!$connection){
+        die("Connection Failed: ".mysqli_connect_error());
+    }
+?>
